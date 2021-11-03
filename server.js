edfe4ee4b5f482ba5f99ea422a8fb23671cd1ee5
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-11-03 18:01:01 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<angular-frontend>'));
+app.use(express.static(__dirname + '/dist/<angularfrontend>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<angular-frontend>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<angularfrontend>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
