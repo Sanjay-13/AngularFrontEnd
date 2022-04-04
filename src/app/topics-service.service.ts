7b71f2c0c17915e9d8e5e67e8f6949553fe56c91
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2022-04-03 21:39:42 -0500

    Update topics-service.service.ts

--- src/app/topics-service.service.ts
+++ src/app/topics-service.service.ts
@@ -8,7 +8,7 @@ import { Topics } from './topics';
 })
 export class TopicsServiceService {
 
-  private baseURL = "http://localhost:8080/api/topics";
+  private baseURL = "/api/topics";
 
   constructor(private httpClient: HttpClient) { }
   
